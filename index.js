e2af7e7615441fe8d7dce1329c983fcc86d994c9
Sumair <sumairqaisar992@gmail.com>
2022-01-11 02:47:50 +0500

    Complete add book feature

--- index.js
+++ index.js
@@ -14,3 +14,25 @@ class Book {
 
 
 let books = []
+
+
+const library = document.querySelector('library')
+const add = document.querySelector('form')
+
+add.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let book = new Book(document.getElementById('title').value,document.getElementById('author').value)
+
+    books.push(book)
+    document.querySelector('form').reset();
+    books.forEach((book)={
+        library.innerHTML += `<div class="book-item">
+        <p>${book.name}</p>
+        <p>${book.author}</p>
+        <button>Remove</button>
+    </div>`
+
+    })
+  
+    console.log(books)
+})
