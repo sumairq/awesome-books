f3f72fb42b1dbf93482b7a797cc65f2d0c650647
Dennis Akagha <d.akagha20@gmail.com>
2022-01-10 23:15:23 +0100

    Refactor index.js to dynamically serve data in ui.

--- index.js
+++ index.js
@@ -13,26 +13,51 @@ class Book {
 }
 
 
-let books = []
+let books = [
+    {
+        title: 'Sam\'s journey in JavaScript',
+        author: 'Sam Doe'
+    },
+    {
+        title: 'My journey as asoftware Developer',
+        author: 'Jane Doe'
+    }
+]
+
+//Creating an element
+const library = document.querySelector('.library');
+
+const createBookElement = (books) => {
+    const bookItems = document.createElement('div');
+    bookItems.innerHTML = `
+    <p>${books.title}</p>
+    <p>${books.author}</p>
+    <button>Remove</button>
+    <hr>
+    `;
+library.appendChild(bookItems);
+}
+
+books.forEach(createBookElement);
 
 
-const library = document.querySelector('library')
+
 const add = document.querySelector('form')
 
-add.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    let book = new Book(document.getElementById('title').value,document.getElementById('author').value)
+// add.addEventListener("submit",(e)=>{
+//     e.preventDefault();
+//     let book = new Book(document.getElementById('title').value,document.getElementById('author').value)
 
-    books.push(book)
-    document.querySelector('form').reset();
-    books.forEach((book)={
-        library.innerHTML += `<div class="book-item">
-        <p>${book.name}</p>
-        <p>${book.author}</p>
-        <button>Remove</button>
-    </div>`
+//     books.push(book)
+//     document.querySelector('form').reset();
+//     books.forEach((book)={
+//         library.innerHTML += `<div class="book-item">
+//         <p>${book.name}</p>
+//         <p>${book.author}</p>
+//         <button>Remove</button>
+//     </div>`
 
-    })
+//     })
   
-    console.log(books)
-})
+//     console.log(books)
+// })
