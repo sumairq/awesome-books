f24fca5d38615c6332a3e0b46f97df2bb4bb6bcb
Sumair <sumairqaisar992@gmail.com>
2022-01-11 00:49:13 +0500

    Add basic structure

---
+++ index.js
@@ -0,0 +1,16 @@
+// Dyanamically modify the dom and add basic Events..
+
+// book object 
+// boooks array
+
+
+
+class Book {
+    constructor(name, author){
+        this.name = name;
+        this.author = author;
+    }
+}
+
+
+let books = []
